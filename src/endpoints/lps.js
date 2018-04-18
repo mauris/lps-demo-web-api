54a7f612a83c0334590a12656d9bb6a07d08fcb5
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-18 14:52:30 +0100

    add api to get examples

--- src/endpoints/lps.js
+++ src/endpoints/lps.js
@@ -1,8 +1,42 @@
 const router = require('express').Router();
 const Parser = require(process.env.LIBRARY_DIR + '/src/parser/Parser');
 const Engine = require(process.env.LIBRARY_DIR + '/src/engine/Engine');
+const fs = require('fs');
 
 module.exports = router;
+
+const examplePrograms = [
+  'mark',
+  'fireSimple',
+  'fireRecurrent',
+  'mapColouring',
+  'trash',
+  'dining'
+];
+
+router.get('/examples', (req, res, next) => {
+  res.json({
+    result: examplePrograms
+  });
+});
+
+router.get('/examples/:id', (req, res, next) => {
+  let programName = examplePrograms[Number(req.params.id)];
+  if (programName === undefined) {
+    next(new Error('Invalid parameter id'));
+    return;
+  }
+  let file = process.env.LIBRARY_DIR + '/examples/' + programName + '.lps'; 
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json({
+      result: data
+    });
+  });
+});
 
 router.post('/execute', (req, res, next) => {
   let source = req.body.source;
